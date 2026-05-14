6c6da6fddc351eee1c8d1320afab659d3fed1a8d
Dhruv Sorout <dhruvsorout0010@gmail.com>
2026-05-14 17:35:54 +0530

    understand try-catch statement

---
+++ week-6/try-catch/index.js
@@ -0,0 +1,12 @@
+function getLength(name){
+    return name.length;
+}
+
+try {
+    const ans = getLength();
+    console.log(ans);
+} catch (e) {
+    console.log("inside catch statement")
+}
+
+console.log("hi there")
